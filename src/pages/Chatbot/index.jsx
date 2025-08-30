092dacdca49c5d4b220d7330d3f46c76e0fdac28
Everton Freitas <evertonfreitas016@hotmail.com>
2025-08-30 16:33:53 -0300

    update chatbot

--- src/pages/Chatbot/index.jsx
+++ src/pages/Chatbot/index.jsx
@@ -11,13 +11,22 @@ function Chatbot() {
   const [input, setInput] = useState("");
   const messagesEndRef = useRef(null);
 
-   useEffect(() => {
+  useEffect(() => {
+    let sessionId = localStorage.getItem("sessionId");
+
+    if (!sessionId) {
+      sessionId = crypto.randomUUID();
+      localStorage.setItem("sessionId", sessionId);
+    }
+  }, []);
+
+  useEffect(() => {
     setMessages([{
       sender: "bot", text: "👋 Olá! Eu sou seu professor virtual de programação. O que você gostaria de aprender hoje?"
     }])
-   }, [])
+  }, [])
 
-   useEffect(() => {
+  useEffect(() => {
     messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
   }, [messages]);
 
@@ -25,13 +34,15 @@ function Chatbot() {
   const sendMessage = async () => {
     if (!input) return;
 
-    
     setMessages((prev) => [...prev, { sender: "user", text: input }]);
     setInput("");
+
     try {
+      const sessionId = localStorage.getItem("sessionId"); 
+
       const res = await axios.post(
-        "http://95.216.142.66:5678/webhook/chatbot",
-        { message: input },
+        "http://95.216.142.66:5678/webhook-test/chatbot",
+        { message: input, sessionId },
         { headers: { "Content-Type": "application/json" } }
       );
 
@@ -47,60 +58,58 @@ function Chatbot() {
         { sender: "bot", text: "Desculpe, não consegui responder." },
       ]);
     }
+};
 
-    
-  };
+return (
+  <ChatContainer>
 
-  return (
-      <ChatContainer>
-        
-        <ChatHistory>
-          {messages.map((m, i) => (
-            <History key={i}>
-              {m.sender === "user" && (
-                <TextUser style={{ textAlign: "right" }}>
-                  <Text  style={{ whiteSpace: "pre-line" }} sender={m.sender}>{m.text}</Text>
-                  <img 
-                    src={userAvatar}
-                    alt="user"
-                    style={{ width:"40px", height: "40px", borderRadius: "6.7px",     alignSelf: "end", marginBottom: "0.2rem"}}
-                  />
-                </TextUser>
-              )}
+    <ChatHistory>
+      {messages.map((m, i) => (
+        <History key={i}>
+          {m.sender === "user" && (
+            <TextUser style={{ textAlign: "right" }}>
+              <Text style={{ whiteSpace: "pre-line" }} sender={m.sender}>{m.text}</Text>
+              <img
+                src={userAvatar}
+                alt="user"
+                style={{ width: "40px", height: "40px", borderRadius: "6.7px", alignSelf: "end", marginBottom: "0.2rem" }}
+              />
+            </TextUser>
+          )}
 
-              {m.sender === "bot" && (
-                <p style={{ textAlign: "left" }}>
-                  
-                  <img
-                    src={botAvatar}
-                    alt="bot"
-                    style={{ width:"40px", height: "40px", borderRadius: "6.7px", alignSelf: "end", marginBottom: "0.2rem"}}
-                  />
-                  <Text style={{ whiteSpace: "pre-line" }} sender={m.sender}>{m.text}</Text>
-                  <div ref={messagesEndRef} />
-                </p>
-                
-              )}
-            </History>
+          {m.sender === "bot" && (
+            <p style={{ textAlign: "left" }}>
 
-             
-          ))}
-        </ChatHistory>
+              <img
+                src={botAvatar}
+                alt="bot"
+                style={{ width: "40px", height: "40px", borderRadius: "6.7px", alignSelf: "end", marginBottom: "0.2rem" }}
+              />
+              <Text style={{ whiteSpace: "pre-line" }} sender={m.sender}>{m.text}</Text>
+              <div ref={messagesEndRef} />
+            </p>
 
-        <Chat>
-          <Input
-            minRows={1}
-            maxRows={8}
-            value={input}
-            onChange={(e) => setInput(e.target.value)}
-            placeholder="Digite sua pergunta..."
-          />
-          <Button onClick={sendMessage}>
-            <PaperPlaneRight size={26} />
-          </Button>
-        </Chat>
-      </ChatContainer>
-  );
+          )}
+        </History>
+
+
+      ))}
+    </ChatHistory>
+
+    <Chat>
+      <Input
+        minRows={1}
+        maxRows={8}
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        placeholder="Digite sua pergunta..."
+      />
+      <Button onClick={sendMessage}>
+        <PaperPlaneRight size={26} />
+      </Button>
+    </Chat>
+  </ChatContainer>
+);
 }
 
 export default Chatbot;
