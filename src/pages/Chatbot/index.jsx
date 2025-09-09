40ebc8ad1a887172f5b28d17ec6095b85f4d9ff1
Everton Freitas <evertonfreitas016@hotmail.com>
2025-09-09 13:05:48 -0300

    Update index.jsx

--- src/pages/Chatbot/index.jsx
+++ src/pages/Chatbot/index.jsx
@@ -112,7 +112,7 @@ function Chatbot() {
       const sessionId = localStorage.getItem("sessionId"); 
 
       const res = await axios.post(
-        "https://lici.app.n8n.cloud/webhook-test/multichat",
+        "https://lici.app.n8n.cloud/webhook/chat",
         { message: messageToSend, sessionId },
         { headers: { "Content-Type": "application/json" } }
       );
